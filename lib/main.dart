a1a9040a01490cf3bd6cd04d4c25d1093fbef4a2
Kurogoma939 <kan.sss.939@gmail.com>
2023-12-02 03:42:23 +0900

    🚀 テストで差分を作る

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'クリック回数の表示',
             ),
             Text(
               '$_counter',
